c928cfc5ea60314d8aab25e40712d3e7fe5b7d59
max-pc <makskms@gmail.com>
2021-12-19 17:30:10 +1000

    Добавьте файлы проекта.

---
+++ TreeVisualizer/MainWindow.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace TreeVisualizer
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox_Control = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.txt = new System.Windows.Forms.TextBox();
+            this.btn_Remove = new System.Windows.Forms.Button();
+            this.btn_Insert = new System.Windows.Forms.Button();
+            this.drawBox = new TreeVisualizer.DrawBox();
+            this.tableLayoutPanel.SuspendLayout();
+            this.groupBox_Control.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.drawBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.ColumnCount = 1;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel.Controls.Add(this.groupBox_Control, 0, 1);
+            this.tableLayoutPanel.Controls.Add(this.drawBox, 0, 0);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Margin = new System.Windows.Forms.Padding(2);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 2;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 81F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(981, 598);
+            this.tableLayoutPanel.TabIndex = 0;
+            // 
+            // groupBox_Control
+            // 
+            this.groupBox_Control.Controls.Add(this.label1);
+            this.groupBox_Control.Controls.Add(this.btn_Search);
+            this.groupBox_Control.Controls.Add(this.txt);
+            this.groupBox_Control.Controls.Add(this.btn_Remove);
+            this.groupBox_Control.Controls.Add(this.btn_Insert);
+            this.groupBox_Control.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox_Control.Location = new System.Drawing.Point(2, 519);
+            this.groupBox_Control.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox_Control.Name = "groupBox_Control";
+            this.groupBox_Control.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox_Control.Size = new System.Drawing.Size(977, 77);
+            this.groupBox_Control.TabIndex = 2;
+            this.groupBox_Control.TabStop = false;
+            this.groupBox_Control.Text = "Управление";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Задайте значение:";
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Location = new System.Drawing.Point(196, 19);
+            this.btn_Search.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(112, 25);
+            this.btn_Search.TabIndex = 3;
+            this.btn_Search.Text = "Найти";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            // 
+            // txt
+            // 
+            this.txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt.Location = new System.Drawing.Point(112, 18);
+            this.txt.Margin = new System.Windows.Forms.Padding(2);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(80, 26);
+            this.txt.TabIndex = 2;
+            this.txt.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btn_Remove
+            // 
+            this.btn_Remove.Location = new System.Drawing.Point(428, 19);
+            this.btn_Remove.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_Remove.Name = "btn_Remove";
+            this.btn_Remove.Size = new System.Drawing.Size(112, 25);
+            this.btn_Remove.TabIndex = 1;
+            this.btn_Remove.Text = "Удалить";
+            this.btn_Remove.UseVisualStyleBackColor = true;
+            this.btn_Remove.Click += new System.EventHandler(this.btn_Remove_Click);
+            // 
+            // btn_Insert
+            // 
+            this.btn_Insert.Location = new System.Drawing.Point(312, 19);
+            this.btn_Insert.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_Insert.Name = "btn_Insert";
+            this.btn_Insert.Size = new System.Drawing.Size(112, 25);
+            this.btn_Insert.TabIndex = 1;
+            this.btn_Insert.Text = "Вставить";
+            this.btn_Insert.UseVisualStyleBackColor = true;
+            this.btn_Insert.Click += new System.EventHandler(this.btn_Insert_Click);
+            // 
+            // drawBox
+            // 
+            this.drawBox.Location = new System.Drawing.Point(3, 3);
+            this.drawBox.Name = "drawBox";
+            this.drawBox.Size = new System.Drawing.Size(975, 511);
+            this.drawBox.TabIndex = 3;
+            this.drawBox.TabStop = false;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(981, 598);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "MainWindow";
+            this.Text = "MainWindow";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.groupBox_Control.ResumeLayout(false);
+            this.groupBox_Control.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.drawBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.GroupBox groupBox_Control;
+        private System.Windows.Forms.Button btn_Remove;
+        private System.Windows.Forms.Button btn_Insert;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.TextBox txt;
+        private System.Windows.Forms.Label label1;
+        private DrawBox drawBox;
+    }
+}
---
+++ TreeVisualizer/MainWindow.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TreeVisualizer
+{
+    public partial class MainWindow : Form
+    {
+        private ITree<int> _avlTree;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            _avlTree = new AVLTree<int>(new TreeConfiguration(circleDiameter: 45, arrowAnchorSize: 5));
+        }
+
+        private void btn_Insert_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txt.Text))
+            {
+                MessageBox.Show("You forgot to enter a value ;)", "Reminder");
+                return;
+            }
+            if (!int.TryParse(txt.Text, out int value))
+            {
+                MessageBox.Show($"Expected value type of {typeof(int)}", "Error");
+                return;
+            }
+            _avlTree.Insert(value);
+            drawBox.Print<AVLTree<int>>(_avlTree);
+            
+        }
+
+        private void btn_Remove_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txt.Text))
+            {
+                MessageBox.Show("You forgot to enter a value ;)", "Reminder");
+                return;
+            }
+            if (!int.TryParse(txt.Text, out int value))
+            {
+                MessageBox.Show($"Expected value type of {typeof(int)}", "Error");
+                return;
+            }
+            _avlTree.Remove(value);
+            drawBox.Print<AVLTree<int>>(_avlTree);
+            
+        }
+
+    }
+}
